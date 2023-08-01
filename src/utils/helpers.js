3efc13e333e748ecd5b389ead58137bd18a490fb
Margarita Diyakonova <margarita.diyakonova@gmail.com>
2023-08-01 12:47:40 +0200

    load project

---
+++ src/utils/helpers.js
@@ -0,0 +1,15 @@
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat('en-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(price / 100)
+}
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type])
+  if (type === 'colors') {
+    unique = unique.flat()
+  }
+
+  return ['all', ...new Set(unique)]
+}
